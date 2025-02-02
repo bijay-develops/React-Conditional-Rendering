e113749d6deb34d09015c29f8738b0619ddede44
Bijay B.K. <bijaybk307@gmail.com>
2025-02-03 04:03:18 +0545

    using if else

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
+import IfElseStatements from './components/IfElseStatements';
 
 function App() {
-    const [isLogged, setIsLogged] = useState(false);
-
-    if (isLogged) {
-        return <h1>Welcome back, User!</h1>;
-    } else {
-        return <button className='btn-login' onClick={() => 
-            setIsLogged(true)}>Login</button>;
-    }
+  return (
+    <div className="App">
+        <h1>Conditional Rendering</h1>
+        <IfElseStatements />
+    </div>
+  );
 }
 
 export default App;
